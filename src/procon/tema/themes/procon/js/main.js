b140b5d0ec7b67e5bf835b0111bbbc8a261b6931
vbrosso <vbrosso@gmail.com>
2016-05-16 15:09:53 -0300

    ajustes no formulario de denuncia js e css

--- src/procon/tema/themes/procon/js/main.js
+++ src/procon/tema/themes/procon/js/main.js
@@ -139,7 +139,7 @@
             $('.tooltip-area').hide();
         });
 
-        var itensForm = $(".formDuvidas").detach();
+        var itensForm = $("#pfg-fieldwrapper").detach();
 
         $('.form-group .btnBuscar, .btnProsseguir').click(function(){
             $('#content #content-core').append(itensForm);
@@ -195,9 +195,126 @@
                 $('input', empresa, data_compra, produto, area).prop('required',false);
             }
         })
-
     }
 
+
+
+
+
+
+
+
+if ($('body').hasClass('portaltype-formfolder') && $('body').hasClass('section-consumidor')) {
+        empresa = $('#archetypes-fieldname-empresa');
+        area = $('#archetypes-fieldname-area');
+        data_compra = $('#archetypes-fieldname-data-da-compra-ou-assinatura-do-contrato');
+        produto = $('#archetypes-fieldname-produto-ou-servico-contratado');
+        btn_enviar = $('input[name=form_submit]');
+        $(empresa, data_compra, produto, area).hide();
+
+        tooltip_url = portal_url + '/consumidor/area';
+        lightbox_url = portal_url + '/consumidor/termos-de-uso-e-politicas-de-privacidade';
+        $.ajax({
+            url: tooltip_url, success: function(tooltip) {
+              tooltip = $(tooltip).find('.contentBody').html();
+              $(area).append("<div class='divGeralTootltip'><a href='javascript:void(0);' class='btnTooltip'>?</a><div class='tooltip-area'>"+tooltip+"<a href='javascript:void(0);' class='fechaTooltip'>FECHAR</a></div></div>");
+            }
+        })
+
+        $.ajax({
+            url: lightbox_url, success: function(lightbox) {
+              lightbox_titulo = $(lightbox).find('.titPage').html();
+              lightbox_text = $(lightbox).find('.contentBody').html();
+              $('body').append("<div class='lightboxGeral'><div class='lightbox-div'><h2>"+lightbox_titulo+"</h2><div class='divScrollLight'>"+lightbox_text+"</div><a href='javascript:void(0);' class='fechaLightbox'>FECHAR</a></div></div>");
+              $(btn_enviar).before("<div class='contentLightbox'><input type='checkbox'>Concordo em disponibilizar as informações contidas em minha reclamação para que sejam divulgadas no site de acordo com os <a href='javascript:void(0);' class='linkLightbox'>Termos de Uso e Políticas de Privacidade.</a></div></div>");
+            }
+        })
+
+        $(document).on('click','.linkLightbox', function(){
+            $('.lightboxGeral').show();
+        });
+         $(document).on('click','.btnTooltip', function(){
+            $('.tooltip-area').toggle();
+        });
+        $(document).on('click','.fechaLightbox', 'body',function(){
+            $('.lightboxGeral').hide();
+        });
+        $(document).on('click','.fechaTooltip', function(){
+            $('.tooltip-area').hide();
+        });
+
+        var itensForm = $("#pfg-fieldwrapper").detach();
+
+        $('.form-group .btnBuscar, .btnProsseguir').click(function(){
+            $('#content #content-core').append(itensForm);
+            $('.form-group').addClass('active');
+            $('.divRedireciona').slideUp();
+
+          //upload plone form gen
+            var file = $("input:file");
+            $.each(file,function(value){
+              if( value > 0 ){
+                 $("#"+file[value].id).parent().parent().hide();
+              }
+            });
+
+            $("input[type='file']").on('change',function(){
+                var id  = $(this).attr('id');
+                console.log(id);
+                $("#"+id).parent().parent().next().show();
+                var nomeArquivo = this.files[0].name;
+                var tamanhoArquivo = this.files[0].size;
+                $("#"+id).after('<a href="#" class="clearImage">Clear</a><br><span style="margin-top:20px;width:600px;height:400px; padding:5px">Nome:'+nomeArquivo+'<br>Tamanho:'+formatar(tamanhoArquivo)+'</spam>');
+            });
+
+            //formata tamanho do arquivo upload
+            var formatar = function formatBytes(bytes,decimals) {
+               if(bytes == 0) return '0 Byte';
+               var k = 1000;
+               var dm = decimals + 1 || 2;
+               var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+               var i = Math.floor(Math.log(bytes) / Math.log(k));
+               return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            };
+
+            $(document).on('click', '.clearImage', function(e) {
+              e.preventDefault();
+              thisParent = $(this).parent()
+              input = $('input', thisParent);
+              $(input).val('');
+              $(this).remove();
+              $('span',thisParent).remove();
+              return false;
+            });
+
+        });
+
+        $(document).on('change', 'input[type=radio][name=deseja-informar-a-empresa]', function(){
+            if (this.value == 'Sim') {
+                $(empresa, data_compra, produto, area).show();
+                $('input', empresa, data_compra, produto, area).prop('required',true);
+            }
+            else if (this.value == 'Não') {
+                $(empresa, data_compra, produto, area).hide();
+                $('input', empresa, data_compra, produto, area).prop('required',false);
+            }
+        })
+    }
+
+    //FORMULARIO NOVO DE DENUNCIA
+    if ($('body').hasClass('section-denuncia')){
+        //CRIA IMPUT COM NUMERO DE PROTOCOLO
+        var inputProtocolo = '<input type="hidden" val="" id="inputProtocolo"/>'
+        $('#content #content-core .formid-formulario-de-denuncia form').append(inputProtocolo);
+
+        var itensForm = $(".pfg-form.formid-formulario-de-denuncia").detach();
+            $('.form-group .btnBuscar').click(function(){
+                $('#content #content-core').append(itensForm);
+                $('.form-group').addClass('active');
+                $('.divRedireciona').slideUp();
+            });
+
+    }
     //categorias CONSUMIDOR.GOV
     $.ajax({
       url: portal_url + '/consumidorjson',
