c112b383df7aac70cda8763840dd1d51eb193e6d
vbrosso <vbrosso@gmail.com>
2016-06-23 14:58:21 -0300

    mascara telefone e margin bottom agradescimento no formulario de adesao

--- src/procon/tema/themes/procon/js/main.js
+++ src/procon/tema/themes/procon/js/main.js
@@ -304,7 +304,7 @@
            $("#valor-da-parcela-clique-ou-toque-aqui-para-inserir-o-texto").maskMoney({showSymbol:true, symbol:"R$", decimal:",", thousands:"."});
            $("#quando-o-produto-ou-servico-apresentou-problema").mask("99/99/9999");
            $("#data-da-compra-ou-assinatura-do-contrato").mask("99/99/9999");
-           $("#telefone, #form-widgets-contato_telefone").mask("(99) 9999-9999?9")
+           $("#telefone, #form-widgets-contato_telefone, #telefone-1").mask("(99) 9999-9999?9")
             .focusout(function (event) {  
                 var target, phone, element;  
                 target = (event.currentTarget) ? event.currentTarget : event.srcElement;  
