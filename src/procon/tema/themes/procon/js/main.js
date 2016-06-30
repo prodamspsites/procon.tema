e639b96422f46425157779216c53154435af6533
mosesjr <shank.sa@gmail.com>
2016-06-30 15:48:07 -0300

    Bugfix de máscara de CNPJ no registro

--- src/procon/tema/themes/procon/js/main.js
+++ src/procon/tema/themes/procon/js/main.js
@@ -1592,16 +1592,16 @@ function testaCNPJ(cnpj) {
 function testaCPF(strCPF) {
 
   if (strCPF.length != 11 || 
-          cpf == "00000000000" || 
-          cpf == "11111111111" || 
-          cpf == "22222222222" || 
-          cpf == "33333333333" || 
-          cpf == "44444444444" || 
-          cpf == "55555555555" || 
-          cpf == "66666666666" || 
-          cpf == "77777777777" || 
-          cpf == "88888888888" || 
-          cpf == "99999999999")
+          strCPF == "00000000000" || 
+          strCPF == "11111111111" || 
+          strCPF == "22222222222" || 
+          strCPF == "33333333333" || 
+          strCPF == "44444444444" || 
+          strCPF == "55555555555" || 
+          strCPF == "66666666666" || 
+          strCPF == "77777777777" || 
+          strCPF == "88888888888" || 
+          strCPF == "99999999999")
               return false;
     var Soma;
     var Resto;
